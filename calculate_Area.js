2fc918c30b8ba67671c6f0ae4a27dafab6769ebb
eldnrck <eldonyabut@gmail.com>
2026-02-12 20:51:16 +0800

    modified the calculate area file to calculate total amount of grocery

--- calculate_Area.js
+++ calculate_Area.js
@@ -1,11 +1,23 @@
-let length;
-let width;
+// let length;
+// let width;
 
-function calculateArea(){
+// function calculateArea(){
 
- length = parseFloat(document.getElementById('length').value);
- width = parseFloat(document.getElementById('width').value);
+//  length = parseFloat(document.getElementById('length').value);
+//  width = parseFloat(document.getElementById('width').value);
 
-let area = length * width;
- document.getElementById('result').innerText = `The area of the rectangle is: ${area}`;
+// let area = length * width;
+//  document.getElementById('result').innerText = `The area of the rectangle is: ${area}`;
+// }
+
+let grocery1;
+let grocery2;
+let grocery3;
+
+function calculateAmount(){
+    grocery1 = parseFloat(document.getElementById('grocery1').value);
+    grocery2 = parseFloat(document.getElementById('grocery2').value);
+    grocery3 = parseFloat(document.getElementById('grocery3').value);
+let totalAmount = grocery1 + grocery2 + grocery3;
+document.getElementById('result').innerText = `The total grocery amount is ${totalAmount}`;
 }
