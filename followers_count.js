44d507b134ec188d51d8d8d02923a2e5623223b2
eldnrck <eldonyabut@gmail.com>
2026-02-16 20:02:03 +0800

    created a product survey HTML and JS

--- followers_count.js
+++ followers_count.js
@@ -1,9 +1,16 @@
 let count = 0; // Initialize count to 0
+let reset = 0; // Created to reset follower count to 0
+
 
 function increaseCount() {
   count++; // Increment the count by 1
    displayCount(); // Display the count
    checkCountValue(); // Check count value and display messages
+}
+
+function resetCount(){
+document.getElementById('countDisplay').innerHTML=reset;
+count = 0;
 }
 
 function displayCount() {
